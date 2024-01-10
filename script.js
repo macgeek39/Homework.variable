f374ffa7ae359f354b0b8e65076c9d1ff5ec959d
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-10 04:20:54 -0600

    Initialized Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+*Declare variables called country, 
+*continent and population and assign their 
+*values according to your own country (population in millions).
+*Log their values to the console.
+*/
