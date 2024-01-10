622c6d3bfa665cbdbacdd4516c3476d090972998
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-10 04:28:25 -0600

    Finished Variables

--- script.js
+++ script.js
@@ -4,3 +4,15 @@
 *values according to your own country (population in millions).
 *Log their values to the console.
 */
+
+// Declare variables called country 
+let country = 'taiwan';
+// Declare variables called continent
+let continent = 'asian';
+// Declare variables called population
+let population = '23,570,000';
+
+//Log their values to the console
+console.log (country);
+console.log(continent);
+console.log(population);
